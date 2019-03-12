8163cd1df3369b36b751662a25996ac29545f99f
gindoc <735506583@qq.com>
2019-03-12 15:22:06 +0800

    删除无用代码

--- app/src/main/java/com/a3xh1/jetpackex/view/auth/AuthActivity.kt
+++ app/src/main/java/com/a3xh1/jetpackex/view/auth/AuthActivity.kt
@@ -1,10 +1,10 @@
 package com.a3xh1.jetpackex.view.auth
 
-import android.os.Bundle
 import androidx.navigation.findNavController
 import com.a3xh1.jetpackex.base.view.activity.BaseActivity
 import com.a3xh1.jetpackex.databinding.ActivityAuthBinding
 import com.a3xh1.jetpackex.leak.IMMLeaks
+import com.a3xh1.jetpackex.R
 
 
 /**
@@ -16,11 +16,6 @@ class AuthActivity : BaseActivity<ActivityAuthBinding>() {
     override val layoutId: Int = R.layout.activity_auth
 
     override fun onSupportNavigateUp() = findNavController(R.id.auth_fragment).navigateUp()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        fixFocusedViewLeak(application)           // 可用
-    }
 
     override fun onDestroy() {
         super.onDestroy()
